ba30cfdea9d52690b10e06795a526718b044338c
Mellowdv <69904426+Mellowdv@users.noreply.github.com>
2023-12-11 12:44:51 +0100

    Update plugins.lua, move to lazy.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,21 +1,80 @@
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-	use 'srcery-colors/srcery-vim'
-    use 'hoob3rt/lualine.nvim'
-	use { 'nvim-treesitter/nvim-treesitter', run = 'tsupdate' }
-	use 'nvim-lua/plenary.nvim'
-	use 'nvim-telescope/telescope.nvim'
-	use 'williamboman/mason.nvim'
-	use 'williamboman/mason-lspconfig.nvim'
-	use 'neovim/nvim-lspconfig'
-	use 'hrsh7th/nvim-cmp'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'saadparwaiz1/cmp_luasnip'
-	use 'l3mon4d3/luasnip'
-	use "mfussenegger/nvim-jdtls"
-	use "p00f/clangd_extensions.nvim"
-	use 'junegunn/fzf'
-	use 'junegunn/fzf.vim'
-	use { 'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons' }
-	use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
-end)
+return {
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = 'TSUpdate',
+    },
+
+    {
+        'nvim-lua/plenary.nvim',
+    },
+
+    {
+        'nvim-telescope/telescope.nvim'
+    },
+
+    {
+        'junegunn/fzf',
+    },
+
+    {
+        'junegunn/fzf.vim',
+    },
+
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=release && cmake --build build --config Release && cmake --install build --prefix build',
+    },
+
+    {
+        'nvim-tree/nvim-web-devicons',
+    },
+
+    {
+        'nvim-lualine/lualine.nvim',
+    },
+
+    {
+        'williamboman/mason.nvim',
+    },
+
+    {
+        'williamboman/mason-lspconfig.nvim',
+    },
+
+    {
+        'neovim/nvim-lspconfig',
+    },
+
+    {
+        'hrsh7th/nvim-cmp',
+    },
+
+    {
+        'hrsh7th/cmp-nvim-lsp',
+    },
+
+    {
+        'saadparwaiz1/cmp_luasnip',
+    },
+
+    {
+        'l3mon4d3/luasnip',
+    },
+
+    {
+        'p00f/clangd_extensions.nvim',
+    },
+
+    {
+        'akinsho/bufferline.nvim',
+        version = '*',
+        dependencies = 'nvim-tree/nvim-web-devicons',
+    },
+
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+}
