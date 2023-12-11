f8aa01ce613dc20d2ce51d92bef6e323dd5ed05e
Mellowdv <69904426+Mellowdv@users.noreply.github.com>
2023-12-11 12:44:10 +0100

    Update init.lua, move to lazy.nvim

--- init.lua
+++ init.lua
@@ -1,205 +1,231 @@
-require('plugins')
 local vim = vim
 
--- telescope fuzzy extension and commands
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-require('telescope').load_extension('fzf')
+require("lazy").setup("plugins")
 
-local builtin = require('telescope.builtin')
+require("telescope").setup {
+    defaults = {
+        wrap_results = true,
+    }
+}
+
+require("telescope").load_extension("fzf")
+
+local builtin = require("telescope.builtin")
 vim.keymap.set('n', 'ff', builtin.find_files, {})
 vim.keymap.set('n', 'fg', builtin.live_grep, {})
 vim.keymap.set('n', 'fb', builtin.buffers, {})
 vim.keymap.set('n', 'fh', builtin.help_tags, {})
 vim.keymap.set('n', '<space>lds', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', '<space>lic', builtin.lsp_incoming_calls, {})
 
-require("mason").setup({
-	automatic_installation = true
-})
-require("mason-lspconfig").setup({
-    ensure_installed = { 'sumneko_lua', 'pylsp', 'jdtls', 'rust_analyzer', 'hls'}
-})
+require("mason").setup()
+require("mason-lspconfig").setup()
 
-require 'nvim-treesitter.configs'.setup {
-	ensure_installed = { "cpp", "lua", "bash", "python", "c", "haskell" },
-	sync_install = false,
-	highlight = {
-		enable = true,
-		disable = {},
-		additional_vim_regex_highlighting = false,
-	},
+require("nvim-treesitter.configs").setup {
+    ensure_installed = { "cpp", "lua", "bash", "python", "c", "haskell", "rust" },
+    sync_install = false,
+    highlight = {
+        enable = true,
+        disable = {},
+        additional_vim_regex_highlighting = false,
+    },
 }
 
 vim.opt.termguicolors = true
-require('nvim-web-devicons').setup {
-    override = {},
-    default = true
-}
 
--- lsp config
-
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
--- on_attach for mapping keys after server attachment to buffer
-local on_attach = function(client, bufnr)
-	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-	-- mappings
-
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-	vim.keymap.set('n', '<space>wl', function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, bufopts)
-	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-	vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
+local luasnip = require'luasnip'
 
+-- Set up nvim-cmp.
+local cmp = require'cmp'
 
-local lsp_flags = {
-	debounce_text_changes = 150,
+cmp.setup({
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end,
+    },
+    window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['Tab'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+      -- that way you will only jump inside the snippet region
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        -- { name = 'vsnip' }, -- For vsnip users.
+        { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+    },
+    {
+        { name = 'buffer' },
+    })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+        { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+    },
+    {
+        { name = 'buffer' },
+    })
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    },
+    {
+        { name = 'cmdline' }
+    })
+})
+
+-- lsp configuration
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local lspconfig = require("lspconfig")
+
+lspconfig.clangd.setup {
+    cmd = {"clangd", "--header-insertion=never"},
+    capabilities = capabilities,
+}
+lspconfig.lua_ls.setup {
+    capabilities = capabilities,
+}
+lspconfig.pylsp.setup {
+    capabilities = capabilities,
+}
+lspconfig.rust_analyzer.setup {
+    capabilities = capabilities,
+}
+lspconfig.gopls.setup {
+    capabilities = capabilities,
 }
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
--- lsp servers config
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-require("clangd_extensions").setup {
-	server = {
-		on_attach = on_attach,
-		flags = lsp_flags,
-		capabilities = capabilities,
-	}
-}
-require('lspconfig')['sumneko_lua'].setup {
-	on_attach = on_attach,
-	flags = lsp_flags,
-	capabilities = capabilities,
-}
-require('lspconfig')['pyright'].setup {
-	on_attach = on_attach,
-	flags = lsp_flags,
-	capabilities = capabilities,
-}
-require('lspconfig')['jdtls'].setup {
-	on_attach = on_attach,
-	flags = lsp_flags,
-	capabilities = capabilities,
-}
-require('lspconfig')['hls'].setup {
-	on_attach = on_attach,
-	flags = lsp_flags,
-	capabilities = capabilities,
-}
-require('lspconfig')['rust_analyzer'].setup {
-	on_attach = on_attach,
-	flags = lsp_flags,
-	capabilities = capabilities,
-}
-require('lspconfig')['asm_lsp'].setup {
-	on_attach = on_attach,
-	flags = lsp_flags,
-	capabilities = capabilities,
-}
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+    vim.lsp.inlay_hint.enable(ev.buf, true)
+  end,
+})
 
--- luasnip setup
-local luasnip = require 'luasnip'
-
--- nvim-cmp setup
-local cmp = require 'cmp'
-cmp.setup {
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['C-Space'] = cmp.mapping.complete(),
-		['<CR>'] = cmp.mapping.confirm {
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		},
-		['<Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { 'i', 's' }),
-		['<S-Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { 'i', 's' }),
-	}),
-	sources = {
-		{ name = 'nvim_lsp' },
-		{ name = 'luasnip' },
-	},
-}
-
--- colorscheme
-vim.cmd [[
-	colorscheme srcery
-]]
-
--- lualine config
 require("lualine").setup {
     options = {
         icons_enabled = true,
-        theme = 'codedark',
+        theme = 'tokyonight'
     }
 }
 
--- bufferline config
-local bufferline = require("bufferline")
-bufferline.setup{
+require("bufferline").setup {
     options = {
-        mode = 'buffers',
-        numbers = 'ordinal',
+        mode = "buffers",
+        numbers = "ordinal",
         color_icons = true,
     },
     highlights = {
         buffer_selected = {
-            italic = false
-        }
+            italic = false,
+        },
     }
 }
 
--- bufferline bindings for jumping to buffers
-vim.keymap.set('n', '<Space>1', ':BufferLineGoToBuffer 1<CR>', { silent = true })
-vim.keymap.set('n', '<Space>2', ':BufferLineGoToBuffer 2<CR>', { silent = true })
-vim.keymap.set('n', '<Space>3', ':BufferLineGoToBuffer 3<CR>', { silent = true })
-vim.keymap.set('n', '<Space>4', ':BufferLineGoToBuffer 4<CR>', { silent = true })
-vim.keymap.set('n', '<Space>5', ':BufferLineGoToBuffer 5<CR>', { silent = true })
-vim.keymap.set('n', '<Space>6', ':BufferLineGoToBuffer 6<CR>', { silent = true })
-vim.keymap.set('n', '<Space>7', ':BufferLineGoToBuffer 7<CR>', { silent = true })
-vim.keymap.set('n', '<Space>8', ':BufferLineGoToBuffer 8<CR>', { silent = true })
-vim.keymap.set('n', '<Space>9', ':BufferLineGoToBuffer 9<CR>', { silent = true })
-vim.keymap.set('n', '<Space>$', ':BufferLineGoToBuffer -1<CR>', { silent = true })
+for buf=1, 9 do
+    local command = string.format(":BufferLineGoToBuffer %d<CR>", buf)
+    local keybind = string.format("<Space>%d", buf)
+    vim.keymap.set('n', keybind, command, { silent = true })
+end
 
+vim.cmd[[colorscheme tokyonight]]
 
--- general Vim options
 vim.opt.ignorecase=true
 vim.opt.smartcase=true
 vim.opt.relativenumber=true
